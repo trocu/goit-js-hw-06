a5e003dd1a1a9c84e5cdb2a1c01c51f59fbb7bf4
Mariusz Trocewicz <throcu@gmail.com>
2023-02-15 01:29:02 +0100

    Add HW 4

---
+++ js/task-04.js
@@ -0,0 +1,22 @@
+const decrementBtn = document.querySelector('[data-action="decrement"]');
+const incrementBtn = document.querySelector('[data-action="increment"]');
+const counter = document.querySelector("#value");
+
+let counterValue = 0;
+decrementBtn.addEventListener("click", () => {
+  counterValue -= 1;
+  counter.innerHTML = counterValue;
+});
+incrementBtn.addEventListener("click", () => {
+  counterValue += 1;
+  counter.innerHTML = counterValue;
+});
+
+// decrementBtn.onclick = function () {
+//   counterValue -= 1;
+//   counter.innerHTML = counterValue;
+// };
+// incrementBtn.onclick = function () {
+//   counterValue += 1;
+//   counter.innerHTML = counterValue;
+// };
