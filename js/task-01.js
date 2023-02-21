e7d8948042303a6d4ce8e9ebd20bd8848b4f7510
Mariusz Trocewicz <throcu@gmail.com>
2023-02-21 16:59:46 +0100

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,7 +1,15 @@
-const categories = document.querySelectorAll(".item");
-const categoryTitle = document.querySelectorAll("h2");
+// const categories = document.querySelectorAll(".item");
+// const categoryTitle = document.querySelectorAll("h2");
+// console.log(`Number of categories: ${categories.length}`);
+// for (let i = 0; i < categories.length; i++) {
+//   console.log(`Category: ${categoryTitle[i].textContent}`);
+//   console.log(`Elements: ${categories[i].querySelectorAll("li").length}`);
+// }
+
+const categories = document.querySelectorAll("li.item");
 console.log(`Number of categories: ${categories.length}`);
-for (let i = 0; i < categories.length; i++) {
-  console.log(`Category: ${categoryTitle[i].textContent}`);
-  console.log(`Elements: ${categories[i].querySelectorAll("li").length}`);
-}
+
+categories.forEach(function (el) {
+  console.log(`Category: ${el.firstElementChild.textContent}`);
+  console.log(`Elements: ${el.lastElementChild.children.length}`);
+});
